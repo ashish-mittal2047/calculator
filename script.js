7aae685d83a112cdd6b25a0b343d2b3a82700580
ashish.mittal2047 <68066532+ashish-mittal2047@users.noreply.github.com>
2024-06-19 16:29:16 +0530

    Define action when we click +, -, *, /

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 let operand1 = 0;
-let operand2, operator;
+let operand2 = null, operator = null;
 let displayValue = 0;
 
 let numbers = document.querySelectorAll('.number');
 let binOperators = document.querySelectorAll('.binary-operators');
+let displayElement = document.querySelectorAll('.display-area');
+
+function updateDisplay() {
+  displayElement.textContent = displayValue;
+}
+
+
+
+numbers.forEach((numberButton) => {
+  numberButton.addEventListener('click', (event) => {
+    const numValue = Number(numberButton.textContent);
+    if (operator === null) {
+      operand1 = 10 * operand1 + numValue;
+      displayValue = operand1.toString();
+      updateDisplay(displayValue);
+    }
+    else {
+      if (operand2 === null)
+        operand2 = 0;
+      operand2 = 10 * operand2 + numValue;
+    }
+  })
+});
+
+binOperators.forEach((operatorButton) => {
+  operatorButton.addEventListener('click', (event) => {
+    const operatorVal = operatorButton.textContent;
+    if (operator === null) {
+      operator = operatorVal;
+    }
+    else {
+      if (operand2 === null) {
+        operator = operatorVal;
+      }
+      else {
+        computeResult();
+      }
+    }
+  });
+});
